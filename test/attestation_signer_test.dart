7f2f0c334a44715814e150597dbb24a779fe87b7
SethDocherty <sddocherty@gmail.com>
2026-03-16 20:10:58 -0400

    refactor: update test imports to use flutter_test package

--- test/attestation_signer_test.dart
+++ test/attestation_signer_test.dart
@@ -1,6 +1,6 @@
 import 'dart:typed_data';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:web3dart/crypto.dart';
 import 'package:web3dart/web3dart.dart';
 
--- test/privy_signer_adapter_test.dart
+++ test/privy_signer_adapter_test.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:web3dart/crypto.dart';
 import 'package:web3dart/web3dart.dart';
 
